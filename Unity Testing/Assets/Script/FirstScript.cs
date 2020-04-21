e8da984ac3eccec7aa9fe887b5751a2bc539c593
Fernando Voltolini de Azambuja <fernando_azambuja@me.com>
2020-04-21 19:52:35 -0400

    Script

--- Unity Testing/Assets/Script/FirstScript.cs
+++ Unity Testing/Assets/Script/FirstScript.cs
@@ -2,12 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class rScript : MonoBehaviour
+public class FirstScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        LogLevel();
+    }
+
+    void LogLevel ()
+    {
+        int level = 20;
+        Debug.Log(level);
     }
 
     // Update is called once per frame
