06245da9233c19fb92e4a329fc554e76b3701691
Fernando Voltolini de Azambuja <fernando_azambuja@me.com>
2020-04-21 20:43:10 -0400

    simple move

---
+++ Unity Testing/Assets/Script/Mover.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update ()
+    {
+        Vector3 newPos = new Vector3(5,2,-2);
+        transform.position = newPos;
+    }
+}
