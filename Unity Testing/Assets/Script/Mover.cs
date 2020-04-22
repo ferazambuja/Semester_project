0b9add776ee75b75e6cbdca86ececafeff41b312
Fernando Voltolini de Azambuja <fernando_azambuja@me.com>
2020-04-21 20:44:52 -0400

    moving sequence

--- Unity Testing/Assets/Script/Mover.cs
+++ Unity Testing/Assets/Script/Mover.cs
@@ -8,7 +8,6 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update ()
     {
-        Vector3 newPos = new Vector3(5,2,-2);
-        transform.position = newPos;
+       transform.position += new Vector3(0.1f,0.1f,0.1f);
     }
 }
