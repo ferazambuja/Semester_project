03d257ec46bb212f73ecc3378ea64ebe529fe6f8
Fernando Voltolini de Azambuja <fernando_azambuja@me.com>
2020-04-24 00:23:41 -0400

    Fix Player facing last position

--- 3DPlatformer/Assets/Scripts/PlayerController.cs
+++ 3DPlatformer/Assets/Scripts/PlayerController.cs
@@ -24,10 +24,21 @@ public class PlayerController : MonoBehaviour
         float xInput = Input.GetAxis("Horizontal");
         float zInput = Input.GetAxis("Vertical");
 
+        // calculate the direction we need to move in
         Vector3 dir = new Vector3(xInput, 0, zInput) * moveSpeed;
         dir.y = rig.velocity.y;
-        
+
+        // set the rigidbody velocity
         rig.velocity = dir;
+
+        // calculate the new forward direction
+        Vector3 facingDir = new Vector3(xInput, 0, zInput);
+
+        // are we holding down our movement buttons?
+        if(facingDir.magnitude > 0)
+        {
+            transform.forward = facingDir;
+        }
     }
 
 }
