b7581c3ddbcc4e6ffb17a69338f754717c1aa0f5
Fernando Voltolini de Azambuja <fernando_azambuja@me.com>
2020-04-23 20:41:05 -0400

    Mono Behavior updates

--- Unity Testing/Assets/Script/TestingScript.cs
+++ Unity Testing/Assets/Script/TestingScript.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class TestingScript : MonoBehaviour
 {
+    void Awake()
+    {
+        Debug.Log("Awake");
+    }
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+       Debug.Log("Start");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Debug.Log("Update every frame");
+    }
+    void FixedUpdate()
+    {
+        //Debug.Log("Fixed Update");   
+    }
+    void LateUpdate()
+    {
+        //Debug.Log("Late Update");    
+    }
+    private void OnEnable() 
+    {
+        Debug.Log("On Enable");    
+    }
+    void OnDisable()
+    {
+        Debug.Log("On Disable");            
     }
 }
