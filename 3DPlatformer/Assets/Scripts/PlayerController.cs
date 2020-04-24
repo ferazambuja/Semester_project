29ed280510dcbd38d9fafaeceb7c1d9385585519
Fernando Voltolini de Azambuja <fernando_azambuja@me.com>
2020-04-24 00:16:23 -0400

    Basic Player Move

---
+++ 3DPlatformer/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+
+    private Rigidbody rig;
+
+    void Awake() 
+    {
+        //get the rigidbody component
+        rig = GetComponent<Rigidbody>();    
+    }
+
+    void Update() 
+    {
+        Move();            
+    }
+    void Move ()
+    {
+        //getting our inputs
+        float xInput = Input.GetAxis("Horizontal");
+        float zInput = Input.GetAxis("Vertical");
+
+        Vector3 dir = new Vector3(xInput, 0, zInput) * moveSpeed;
+        dir.y = rig.velocity.y;
+        
+        rig.velocity = dir;
+    }
+
+}
