c3d36085d8ea635410816d648ebdf460eda952be
Fernando Voltolini de Azambuja <fernando_azambuja@me.com>
2020-04-24 23:50:06 -0400

    GameManager_1

---
+++ 3DPlatformer/Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int score;
+
+    // instance
+    public static GameManager instance;
+
+    void Awake ()
+    {
+        instance = this;
+        
+    }
+    
+
+}
