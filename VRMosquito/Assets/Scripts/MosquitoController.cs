33b06452e1de7a9aca66a21d4b41979c5bbd96ca
Fernando Voltolini de Azambuja <fernando_azambuja@me.com>
2020-04-27 09:49:41 -0400

    Pre Presentation

--- VRMosquito/Assets/Scripts/MosquitoController.cs
+++ VRMosquito/Assets/Scripts/MosquitoController.cs
@@ -1,9 +1,17 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using VRStandardAssets.Utils;
 
-public class MosquitoController : MonoBehaviour
-{
+public class MosquitoController : MonoBehaviour {
+
+    // speed
+    public float speed = 0.5f;
+
+    // min distance from us
+    public float minDistance = 0.55f;
+
     // vr interactive item
     VRInteractiveItem vrIntItem;
 
@@ -12,29 +20,37 @@ public class MosquitoController : MonoBehaviour
 
     // flag to keep track whether this mosquito is moving
     bool isMoving = true;
-    
-    // Start is called before the first frame update
-    void Awake()
-    {
+
+    // target
+    Vector3 target;
+
+	// Use this for initialization
+	void Awake () {
         //grab my vr interactive component
         vrIntItem = GetComponent<VRInteractiveItem>();
 
         //grab the rigidbody component
         rb = GetComponent<Rigidbody>();
-    }
+
+        //set target
+        target = Camera.main.transform.position;
+
+        //make the mosquito look at us
+        transform.LookAt(target);
+	}
 
     // when our game object is enabled
     void OnEnable()
     {
-        vrIntItem.OnClick += HandleClick;    
+        vrIntItem.OnClick += HandleClick;
     }
 
-   // when our game object is disabled
+    // when our game object is disabled
     void OnDisable()
     {
         vrIntItem.OnClick -= HandleClick;
     }
-    
+
     // this is called when the mosquito is clicked on
     void HandleClick()
     {
@@ -51,6 +67,29 @@ public class MosquitoController : MonoBehaviour
             isMoving = false;            
         }
     }
-        
-    
+
+    // Update is called once per frame
+    void Update () {
+		// check that we are moving
+        if(isMoving)
+        {
+            //calculate distance from target
+            float distance = Vector3.Distance(transform.position, target);
+
+            //check min distance
+            if(distance <= minDistance)
+            {
+                //we stop!
+                isMoving = false;
+            }
+            else
+            {
+                //calculate movement step: v = d / t --> d = v * t
+                float movementStep = speed * Time.deltaTime;
+
+                // move on that step
+                transform.position = Vector3.MoveTowards(transform.position, target, movementStep);
+            }
+        }
+	}
 }
