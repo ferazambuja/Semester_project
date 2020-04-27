2f21f1b888435e4abe85fd9ee8ae21e4b542783b
Fernando Voltolini de Azambuja <fernando_azambuja@me.com>
2020-04-27 01:14:36 -0400

    First move

--- VRGameDev/Assets/Scripts/PlatformController.cs
+++ VRGameDev/Assets/Scripts/PlatformController.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class PlatformController : MonoBehaviour
 {
+    //Movement Target
+    public Vector3 target;
+
+    //Speed
+    public float speed = 1;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +21,15 @@ public class PlatformController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //calculate the distance from target
+        float distance = Vector3.Distance(transform.position, target);
+        //have we arrived?
+            if (distance > 0)
+            {
+                //calculate how much we need to move (step) d = v * t
+                float step = speed * Time.deltaTime;
+                //move by that step
+                transform.position = Vector3.MoveTowards(transform.position, target, step);
+            }
     }
 }
