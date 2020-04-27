3b070648bdb7940a543c3c405723bf39b52e244e
Fernando Voltolini de Azambuja <fernando_azambuja@me.com>
2020-04-27 02:08:21 -0400

    VR Camera  Tool

--- VRGameDev/Assets/Scripts/PlatformController.cs
+++ VRGameDev/Assets/Scripts/PlatformController.cs
@@ -4,32 +4,80 @@ using UnityEngine;
 
 public class PlatformController : MonoBehaviour
 {
-    //Movement Target
-    public Vector3 target;
+    // destinations / targets
+    public Transform[] targets;
 
     //Speed
     public float speed = 1;
 
+    // flag that sets whether we are moving or not
+    bool isMoving = false;
 
+    // next destination index
+    int nextIndex;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // set the player to the first target
+        transform.position = targets[0].position;
+
+        // next destination is 1
+        nextIndex = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //calculate the distance from target
-        float distance = Vector3.Distance(transform.position, target);
-        //have we arrived?
-            if (distance > 0)
+        //check for input
+        HandleInput();
+
+        //move our plataform
+        HandleMovement();
+    }
+    //take care of input
+        void HandleInput()
+    {
+        //check for Fire1 axis
+        if(Input.GetButtonDown("Fire1"))
+        {
+            // negate isMoving
+            isMoving = !isMoving;
+        }
+    }
+    //take care of movement
+    void HandleMovement()
+    {
+        // if we are not moving, exit
+        if (!isMoving) return;
+
+        // calculate the distance from target
+        float distance = Vector3.Distance(transform.position, targets[nextIndex].position);
+
+        // have we arrived?
+        if (distance > 0)
+        {
+
+            // calculate how much we need to move (step) d = v * t
+            float step = speed * Time.deltaTime;
+
+            // move by that step
+            transform.position = Vector3.MoveTowards(transform.position, targets[nextIndex].position, step);
+        }
+        // if we have arrived we should update nextIndex
+        else
+        {
+            // next index is increased by 1
+            nextIndex++;
+
+            // array element index starts at 0 and goes all the way to length-1
+            if(nextIndex == targets.Length)
             {
-                //calculate how much we need to move (step) d = v * t
-                float step = speed * Time.deltaTime;
-                //move by that step
-                transform.position = Vector3.MoveTowards(transform.position, target, step);
+                nextIndex = 0;
             }
+
+            //stop moving
+            isMoving = false;
+        }
     }
 }
